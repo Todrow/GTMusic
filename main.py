b158d2e3bba126a352c90c67105996f9e09f4e12
Gl0balRak <86263607+Gl0balRak@users.noreply.github.com>
2021-12-24 22:33:31 +0300

    Update main.py
    Main script

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+import discord
+
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print("Started")
+
+    async def on_ready(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+client = MyClient()
+client.run()
