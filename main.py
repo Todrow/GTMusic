459745c04cbcc59c3d61d7d3aa02df719e405ee6
Gl0balRak <86263607+Gl0balRak@users.noreply.github.com>
2021-12-24 23:47:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ import discord
 import re
 
 
+TEXT_SERVER_ID = 924019353203142736
+
+
 # Making Client class
 class Client(discord.Client):
+    async def docommand(self, message):
+        await self.sent_message(message.content)
 
-    actions = {} # Put here function that will execute if needed message will be received
-    """
-    Example:
-
-    actions = {"!*": f}
-
-    Function "f" will be executed if bot received a message, that starts with "!".
-    Make sure that the only one argument that will be given to the function will
-    be the message, that called a function.
-    """
+    async def sent_message(self, text):
+        await self.get_channel(TEXT_SERVER_ID).send(text)
 
     async def on_ready(self):
         print("Started")
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+
+        self.actions = {"^!*": self.docommand} # Put here function that will execute if needed message will be received
+        """
+        Example:
+
+        actions = {"!*": f}
+
+        Function "f" will be executed if bot received a message, that starts with "!".
+        Make sure that the only one argument that will be given to the function will
+        be the message, that called a function.
+        """
+
         text = message.content
 
-        for item in actions:
-            if re.match(item.key, text):
-                item.value(message)
+        for key, value in self.actions.items():
+            if re.match(key, text) != '':
+                await value(message)
 
 # Getting token
 with open("C:/key/key.txt", "r") as f: token = f.read();
